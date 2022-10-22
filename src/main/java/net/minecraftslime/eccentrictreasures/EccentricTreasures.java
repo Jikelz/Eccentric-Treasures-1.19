78b287c31aa035457fe1900fb10da59fdde8049e
jsh05 <jsh05@DESKTOP-UPONO85>
2022-10-21 22:57:27 -0400

    Initial commit

---
+++ src/main/java/net/minecraftslime/eccentrictreasures/EccentricTreasures.java
@@ -0,0 +1,15 @@
+package net.minecraftslime.eccentrictreasures;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// Very important comment
+public class EccentricTreasures implements ModInitializer {
+	public static final String MOD_ID = "eccentrictreasures";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/minecraftslime/eccentrictreasures/EccentricTreasuresClient.java
@@ -0,0 +1,10 @@
+package net.minecraftslime.eccentrictreasures;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EccentricTreasuresClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
