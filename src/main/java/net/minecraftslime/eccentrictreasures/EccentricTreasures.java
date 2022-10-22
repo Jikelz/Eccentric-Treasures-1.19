ada0075454d21eceeb099a14996327a78773d75b
jsh05 <jsh05@DESKTOP-UPONO85>
2022-10-21 22:59:02 -0400

    slime was somewhere

--- src/main/java/net/minecraftslime/eccentrictreasures/EccentricTreasures.java
+++ src/main/java/net/minecraftslime/eccentrictreasures/EccentricTreasures.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// Very important comment
+// Slime was here
 public class EccentricTreasures implements ModInitializer {
 	public static final String MOD_ID = "eccentrictreasures";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
